137588e4d51216a8e4ab8fa79e96d75c5de5a8c3
YilingCAI <67104963+YilingCAI@users.noreply.github.com>
2024-10-04 10:38:16 +0200

    Update chapter-1.sh

--- scripts-by-chapter/chapter-1.sh
+++ scripts-by-chapter/chapter-1.sh
@@ -1,3 +1,14 @@
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 echo "***************************************************"
 echo "********* CHAPTER 1 - STARTED AT $(date) **********"
 echo "***************************************************"
